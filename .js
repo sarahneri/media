21890e499940ee11601452d3dd0a87af0fb56994
Sarah Neri <105995227+sarahneri@users.noreply.github.com>
2024-03-02 23:13:04 -0300

    media.js

---
+++ .js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Média</title>
+</head>
+<body>
+	
+<script>
+   let nota1 = Number(prompt("Entre com a primeira nota"));
+   let nota2 = Number(prompt("Entre com a segunda nota"));
+   let nota3 = Number(prompt("Entre com a terceira nota"));
+
+   let soma = nota1 + nota2 + nota3
+   let média = soma /3;
+
+   alert (" A média final é:　" + média);
+
+
+</script>
+
+
+</body>
+</html>
